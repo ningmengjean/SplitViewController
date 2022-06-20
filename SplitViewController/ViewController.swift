76b6ecec77a625a6a037a3d4f0abee38343a6a45
zhuxiaojin <zhuxiaojin@eyou.net>
2022-06-20 09:54:12 +0800

    Initial Commit

---
+++ SplitViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitViewController
+//
+//  Created by 朱晓瑾 on 2022/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
